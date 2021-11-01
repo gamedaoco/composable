93eb300decb4c7064fb2f402bf7f503633947765
haroldsphinx <adedayoakinpelu@gmail.com>
2021-11-01 11:35:50 +0100

    Build Faucet Server

---
+++ .github/actions/deploy/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+mkdir ~/.ssh
+echo $VAULT_PASS > /vault_password_file.txt
+ansible-vault decrypt \
+--vault-password-file=/vault_password_file.txt \
+id_rsa-encrypted \
+--output=~/.ssh/id_rsa
+chmod 0600 ~/.ssh/id_rsa
+ansible-playbook --vault-password-file=/vault_password_file.txt \
+-i ansible/hosts \
+ansible/deploy.yml
