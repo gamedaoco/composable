b88a671695c985c09e27c02b5668456b964ebfad
dzmitry.lahoda <dzmitry@lahoda.pro>
2021-11-13 22:23:11 +0200

    all tests ported

--- integration-tests/src/cross_chain_transfer.rs
+++ integration-tests/src/cross_chain_transfer.rs
@@ -61,9 +61,112 @@ fn transfer_to_relay_chain() {
                 ),
                 4_600_000_000);
             assert_ok!(transfered);
+
+            let remaining = picasso_runtime::Tokens::free_balance(
+                CurrencyId::PICA, &AccountId::from(ALICE));
+
+            assert_eq!(remaining, 200 * PICA - 3 * PICA);
     });
+
+    KusamaRelay::execute_with(|| {
+		assert_eq!(
+			kusama_runtime::Balances::free_balance(&AccountId::from(BOB)),
+			2999893333340 // 3 * BSX - fee
+		);
+	});
 }
 
+#[test]
+fn transfer_from_dali() {
+	TestNet::reset();
+
+    // modify our registry to handle this
+	// Picasso::execute_with(|| {
+	// 	assert_ok!(picasso_runtime::AssetRegistry::set_location(
+	// 		picasso_runtime::Origin::root(),
+	// 		1,
+	// 		picasso_runtime::AssetLocation(MultiLocation::new(1, X2(Parachain(3000), GeneralKey(vec![0, 0, 0, 0]))))
+	// 	));
+	// });
+
+	Dali::execute_with(|| {
+		assert_ok!(dali_runtime::XTokens::transfer(
+			dali_runtime::Origin::signed(ALICE.into()),
+			0,
+			3 * BSX,
+			Box::new(
+				MultiLocation::new(
+					1,
+					X2(
+						Junction::Parachain(2000),
+						Junction::AccountId32 {
+							id: BOB,
+							network: NetworkId::Any,
+						}
+					)
+				)
+				.into()
+			),
+			399_600_000_000
+		));
+		assert_eq!(
+			picasso_runtime::Balances::free_balance(&AccountId::from(ALICE)),
+			200 * BSX - 3 * BSX
+		);
+	});
+
+	Picasso::execute_with(|| {
+		assert_eq!(
+			picasso_runtime::Tokens::free_balance(1, &AccountId::from(BOB)),
+			3 * BSX
+		);
+	});
+}
+
+
+#[test]
+fn transfer_insufficient_amount_should_fail() {
+	TestNet::reset();
+
+	// Picasso::execute_with(|| {
+	// 	assert_ok!(picasso_runtime::AssetRegistry::set_location(
+	// 		picasso_runtime::Origin::root(),
+	// 		1,
+	// 		picasso_runtime::AssetLocation(MultiLocation::new(1, X2(Parachain(3000), GeneralKey(vec![0, 0, 0, 0]))))
+	// 	));
+	// });
+
+	Dali::execute_with(|| {
+		assert_ok!(dali_runtime::XTokens::transfer(
+			dali_runtime::Origin::signed(ALICE.into()),
+			0,
+			1_000_000 - 1,
+			Box::new(
+				MultiLocation::new(
+					1,
+					X2(
+						Junction::Parachain(2000),
+						Junction::AccountId32 {
+							id: BOB,
+							network: NetworkId::Any,
+						}
+					)
+				)
+				.into()
+			),
+			399_600_000_000
+		));
+		assert_eq!(
+			dali_runtime::Balances::free_balance(&AccountId::from(ALICE)),
+			199999999000001
+		);
+	});
+
+	Picasso::execute_with(|| {
+		// Xcm should fail therefore nothing should be deposit into beneficiary account
+		assert_eq!(picasso_runtime::Tokens::free_balance(1, &AccountId::from(BOB)), 0);
+	});
+}
 
 
 
