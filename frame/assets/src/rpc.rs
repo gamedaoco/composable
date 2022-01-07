6920ba05aa8b2e25293bb8418b7a3e89de256015
Ben Luelo <benluelo@hotmail.com>
2022-01-07 15:00:38 -0500

    Implement basic balance_of functionality (untested)

--- frame/assets/src/rpc.rs
+++ frame/assets/src/rpc.rs
@@ -1,5 +1,6 @@
 use crate::runtime_api::AssetsApi as AssetsRuntimeApi;
-use jsonrpc_core::{Error as RpcError, ErrorCode, Result};
+use codec::Codec;
+use jsonrpc_core::{Error as RpcError, ErrorCode, Result as RpcResult};
 use jsonrpc_derive::rpc;
 use sp_api::ProvideRuntimeApi;
 use sp_blockchain::HeaderBackend;
@@ -7,17 +8,22 @@ use sp_runtime::{generic::BlockId, traits::Block as BlockT};
 use std::sync::Arc;
 
 #[rpc]
-pub trait AssetsApi<BlockHash> {
-	// #[rpc(name = "sumStorage_getSum")]
-	// fn get_sum(&self, at: Option<BlockHash>) -> Result<u32>;
+pub trait AssetsApi<BlockHash, AssetId, AccountId, Balance> {
+	#[rpc(name = "assets_balanceOf")] // , returns = "Balance"
+	fn balance_of(
+		&self,
+		at: Option<BlockHash>,
+		currency: AssetId,
+		account: AccountId,
+	) -> RpcResult<Option<Balance>>;
 }
 
 /// A struct that implements the `SumStorageApi`.
-pub struct Assets<C, M> {
+pub struct Assets<C, Block> {
 	// If you have more generics, no need to SumStorage<C, M, N, P, ...>
 	// just use a tuple like SumStorage<C, (M, N, P, ...)>
 	client: Arc<C>,
-	_marker: std::marker::PhantomData<M>,
+	_marker: std::marker::PhantomData<Block>,
 }
 
 impl<C, M> Assets<C, M> {
@@ -44,14 +50,42 @@ impl<C, M> Assets<C, M> {
 // 	}
 // }
 
-impl<C, Block> AssetsApi<<Block as BlockT>::Hash> for Assets<C, Block>
+impl<C, Block, AssetId, AccountId, Balance>
+	AssetsApi<<Block as BlockT>::Hash, AssetId, AccountId, Balance>
+	for Assets<C, (Block, AssetId, AccountId, Balance)>
 where
 	Block: BlockT,
+	AssetId: Codec + Send + Sync + 'static,
+	AccountId: Codec + Send + Sync + 'static,
+	Balance: Codec + Send + Sync + 'static,
 	C: Send + Sync + 'static,
 	C: ProvideRuntimeApi<Block>,
 	C: HeaderBackend<Block>,
-	C::Api: AssetsRuntimeApi<Block>,
+	C::Api: AssetsRuntimeApi<Block, AssetId, AccountId, Balance>,
 {
+	fn balance_of(
+		&self,
+		at: Option<<Block as BlockT>::Hash>,
+		asset_id: AssetId,
+		account_id: AccountId,
+	) -> RpcResult<Option<Balance>> {
+		let api = self.client.runtime_api();
+		let at = BlockId::hash(at.unwrap_or_else(|| {
+			// If the block hash is not supplied assume the best block.
+			self.client.info().best_hash
+		}));
+
+		let runtime_api_result = api.balance_of(&at, asset_id, account_id);
+		// TODO(benluelo): Review what error message & code to use
+		runtime_api_result.map_err(|e| {
+			RpcError {
+				code: ErrorCode::ServerError(9876), // No real reason for this value
+				message: "Something wrong".into(),
+				data: Some(format!("{:?}", e).into()),
+			}
+		})
+	}
+
 	// fn get_sum(&self, at: Option<<Block as BlockT>::Hash>) -> Result<u32> {
 	// 	let api = self.client.runtime_api();
 	// 	let at = BlockId::hash(at.unwrap_or_else(||
--- frame/assets/src/runtime_api.rs
+++ frame/assets/src/runtime_api.rs
@@ -2,10 +2,17 @@
 #![allow(clippy::too_many_arguments)]
 #![allow(clippy::unnecessary_mut_passed)]
 
+use codec::{Decode, Encode};
+
 // Here we declare the runtime API. It is implemented it the `impl` block in
 // runtime amalgamator file (the `runtime/src/lib.rs`)
 sp_api::decl_runtime_apis! {
-	pub trait AssetsApi {
-		// fn get_sum() -> u32;
+	pub trait AssetsApi<AssetId, AccountId, Balance>
+	where
+		AssetId: Encode + Decode,
+		AccountId: Encode + Decode,
+		Balance: Encode + Decode,
+	{
+		fn balance_of(asset_id: AssetId, account_id: AccountId) -> Option<Balance>;
 	}
 }
